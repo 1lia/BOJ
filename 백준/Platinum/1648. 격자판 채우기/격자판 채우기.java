9a2d4d99d311d7051544562c2e4279fb139a93a1
1lia <79691058+1lia@users.noreply.github.com>
2023-03-24 23:10:06 +0900

    [Platinum III] Title: 격자판 채우기, Time: 352 ms, Memory: 41212 KB -BaekjoonHub

---
+++ 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N;
+	public static int M;
+	public static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		if(((N * M) & 1) == 1) {
+			System.out.println(0);
+			System.exit(0);
+		}
+			
+		dp = new int[(N + 1)  * (M + 1)][(1 << (M+1)) - 1];
+
+		System.out.println(dfs(0, 0));
+	}
+
+	static int dfs(int cnt, int state) {
+		if (cnt >= N * M) {
+			if (cnt == N * M && state == 0)
+				return 1;
+			return 0;
+		}
+
+		int result = dp[cnt][state];
+		if (result != 0)
+			return result;
+		
+		result = 0;
+		// 이미 채워져있으면 상태한칸이동
+		if ((state & (1 << 0)) > 0) {
+			result += dfs(cnt + 1, (state >> 1));
+		}
+		// 채우는경우
+		else {
+//			가로로 채울때 옆에가 비어있거나 마지막칸이 아니어야함
+			if (cnt % M < (M - 1) && (state & (1 << 1)) == 0) {
+				result += dfs(cnt + 2, state >> 2);
+			}
+//			세로로 채울때 아래는 무조건 비어있음
+			result += dfs(cnt + 1, (state >> 1) | (1 << (M - 1)));
+		}
+		result %= 9901;
+		return dp[cnt][state] = result;
+	}
+}
