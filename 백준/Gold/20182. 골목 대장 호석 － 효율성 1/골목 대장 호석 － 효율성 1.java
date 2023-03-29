f1fb8f9dee2eb213fca2ec6509882caaafd3a4e7
1lia <79691058+1lia@users.noreply.github.com>
2023-03-29 09:18:39 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 1212 ms, Memory: 70976 KB -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
@@ -1,19 +1,21 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
 	public static ArrayList<Node>[] g;
-	public static PriorityQueue<Integer> q = new PriorityQueue<>();
+	public static Queue<Integer> q = new ArrayDeque<>();
 	public static int[] distance;
 	public static int end;
+	public static int size;
 
 	public static void main(String[] args) throws Exception {
 		int N = readInt();
 		int M = readInt();
 		int start = readInt();
 		end = readInt();
-		int size = readInt();
+		size = readInt();
 		g = new ArrayList[N + 1];
 		distance = new int[N + 1];
 
@@ -56,11 +58,11 @@ public class Main {
 
 		while (!q.isEmpty()) {
 			int v = q.poll();
-
+			
 			// 꺼낸노드에 연결된 그래프 탐색
 			for (int i = 0; i < g[v].size(); i++) {
 				Node nextNode = g[v].get(i);
-
+				
 				// 다음노드의 최단거리보다 현재노드거리 + 다음노드까지의 거리가 작다면
 				if (nextNode.cost <= maxcost) {
 					if (distance[nextNode.v] > distance[v] + nextNode.cost) {
