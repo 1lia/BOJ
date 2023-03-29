2a7fce04950a2d60440e0d32bfa1212bfcd8debd
1lia <79691058+1lia@users.noreply.github.com>
2023-03-29 15:53:26 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2352 ms, Memory: 965768 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		Deque<Pos> q = new ArrayDeque<>();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int a = 0;
+		st = new StringTokenizer(bf.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(bf.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < N; i++) {
+			a = Integer.parseInt(st.nextToken());
+			while(!q.isEmpty() && q.peekLast().num >= a) {
+				q.pollLast();
+			}
+			q.addLast(new Pos(a , i));
+			while(!q.isEmpty() && q.peekFirst().count <= i - L) {
+				q.pollFirst();
+			}
+			sb.append(q.peekFirst().num).append(" ");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
+
+class Pos{
+	int num;
+	int count;
+	Pos(int num , int count){
+		this.num = num;
+		this.count = count;
+	}
+}
