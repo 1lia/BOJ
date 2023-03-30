e027e47eb9f92b43a6220c38226a1422bac0c3c6
1lia <79691058+1lia@users.noreply.github.com>
2023-03-30 14:02:46 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 1156 ms, Memory: 213568 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		Queue<Node> q = new ArrayDeque<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int[][] dxy = {{1,-1,0,0},{0,0,1,-1}};
+		int dx = 0;
+		int dy = 0;
+		Node node = null;
+		int cnt = 1;
+		
+		while(true) {
+			int N = Integer.parseInt(br.readLine());
+			if(N == 0)
+				break;
+			int[][] arr = new int[N][N];
+			int[][] distance = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(distance[i], Integer.MAX_VALUE);
+			}
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			q.clear();
+			q.offer(new Node(0,0,arr[0][0]));
+			
+			while(!q.isEmpty()) {
+				node = q.poll();
+				for (int i = 0; i < 4; i++) {
+					dx = node.x + dxy[0][i];
+					dy = node.y + dxy[1][i];
+					if(dx < N && dx >= 0 && dy < N && dy >= 0) {
+						if(node.cost + arr[dx][dy] < distance[dx][dy]) {
+							distance[dx][dy] = node.cost + arr[dx][dy];
+							q.offer(new Node(dx,dy,node.cost + arr[dx][dy]));
+						}
+					}
+				}
+			}
+			sb.append("Problem ").append(cnt++).append(": ").append(distance[N-1][N-1]).append('\n');
+		}
+		System.out.println(sb.toString());
+		
+	}
+}
+
+class Node{
+	int x;
+	int y;
+	int cost;
+	public Node(int x, int y, int cost) {
+		this.x = x;
+		this.y = y;
+		this.cost = cost;
+	}
+}
