22b658fc6510089f03f2dca427d02c8a461f8c73
1lia <79691058+1lia@users.noreply.github.com>
2023-04-30 13:16:35 +0900

    [Gold IV] Title: 알고스팟, Time: 120 ms, Memory: 14688 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Deque<Pos> q = new ArrayDeque<>();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[M][N];
+		int[][] visit = new int[M][N];
+		
+		for (int i = 0; i < M; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = s.charAt(j) - '0';
+				visit[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		int[][] dxy = {{1,-1,0,0} ,{ 0 , 0 , 1 , -1 }};
+		q.offer(new Pos(0 , 0));
+		visit[0][0] = 0;
+		while(!q.isEmpty()) {
+			Pos p = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int dx = p.x + dxy[0][i];
+				int dy = p.y + dxy[1][i];
+				if(0 <= dx && dx < M && 0 <= dy && dy < N) {
+					if(visit[p.x][p.y] + map[dx][dy] < visit[dx][dy]) {
+						visit[dx][dy] =  visit[p.x][p.y] + map[dx][dy];
+						if(map[dx][dy] == 1) {
+							q.offer(new Pos(dx , dy));
+						} else {
+							q.offerLast(new Pos(dx , dy));
+						}
+					}
+				}
+			}
+		}	
+		System.out.println(visit[M-1][N-1]);
+	}
+}
+
+class Pos {
+	int x = 0;
+	int y = 0;
+	public Pos(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
