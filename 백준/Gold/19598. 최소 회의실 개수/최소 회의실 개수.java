a0b4ae8f257ce66a9b15fcb7ffe93644d397417a
1lia <79691058+1lia@users.noreply.github.com>
2023-07-16 23:12:02 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 444 ms, Memory: 23716 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -3,78 +3,69 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<T> q = new PriorityQueue<>(new Comparator<T>() {
-            @Override
-            public int compare(T o1, T o2) {
-                if(o1.e > o2.e){
-                    return 1;
-                } else {
-                    return -1;
-                }
-            }
-        });
+	public static void main(String[] args) throws Exception {
+		PriorityQueue<Long> q = new PriorityQueue<>();
+		int N = (int) readLong();
+		T[] arr = new T[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = new T(readLong(), readLong());
+		}
 
-        int N = (int) readLong();
-        T[] arr = new T[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = new T(readLong() , readLong());
-        }
+		Comparator<T> com = new Comparator<T>() {
+			@Override
+			public int compare(T o1, T o2) {
+				if (o1.s > o2.s) {
+					return 1;
+				} else if(o1.s < o2.s){
+					return -1;
+				} else {
+					if(o1.e > o1.e) {
+						return 1;
+					} else if(o1.e > o1.e) {
+						return -1;
+					} else {
+						return 0;
+					}
+				}
+			}
+		};
+		Arrays.sort(arr, com);
+		q.offer(arr[0].e);
+		for (int i = 1; i < N; i++) {
+			if (q.peek() <= arr[i].s) {
+				q.poll();
+			}
+			q.offer(arr[i].e);
+		}
 
-        Comparator<T> com = new Comparator<T>() {
-            @Override
-            public int compare(T o1, T o2) {
-                if(o1.s > o2.s){
-                    return 1;
-                } else{
-                    return -1;
-                }
-            }
-        };
-        Arrays.sort(arr , com);
-        long s = 0;
-        long res = 0;
-        for (int i = 0; i < N; i++) {
-            s = arr[i].s;
+		System.out.println(q.size());
+	}
 
-            q.offer(arr[i]);
-            while(!q.isEmpty()){
-                if(q.peek().e <= s){
-                    q.poll();
-                } else{
-                    break;
-                }
-            }
-            res = Math.max(res , q.size());
-        }
+	public static long readLong() throws Exception {
+		long val = 0;
+		long c = System.in.read();
+		while (c <= ' ') {
+			c = System.in.read();
+		}
+		boolean flag = (c == '-');
+		if (flag)
+			c = System.in.read();
+		do {
+			val = 10 * val + c - 48;
+		} while ((c = System.in.read()) >= 48 && c <= 57);
 
-        System.out.println(res);
-    }
-
-    public static long readLong() throws Exception {
-        long val = 0;
-        long c = System.in.read();
-        while (c <= ' ') {
-            c = System.in.read();
-        }
-        boolean flag = (c == '-');
-        if (flag) c = System.in.read();
-        do {
-            val = 10 * val + c - 48;
-        } while ((c = System.in.read()) >= 48 && c <= 57);
-
-        if (flag) return -val;
-        return val;
-    }
+		if (flag)
+			return -val;
+		return val;
+	}
 }
 
-class T{
-    long s;
-    long e;
+class T {
+	long s;
+	long e;
 
-    public T(long s, long e) {
-        this.s = s;
-        this.e = e;
-    }
+	public T(long s, long e) {
+		this.s = s;
+		this.e = e;
+	}
 }
