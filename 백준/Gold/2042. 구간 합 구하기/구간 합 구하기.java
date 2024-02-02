d7b881df1336a94048a75b643157a7e17ebe617f
1lia <79691058+1lia@users.noreply.github.com>
2024-02-02 20:58:07 +0900

    [Gold I] Title: 구간 합 구하기, Time: 412 ms, Memory: 30768 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -3,17 +3,17 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		int N = readInt();
 		int M = readInt() + readInt();
-		long[] arr = new long[N + 1];
-		for (int i = 1; i <= N; i++) {
-			arr[i] = readLong();
-		}
 		Seg seg = new Seg(N);
-		seg.init(arr, 1, 1, N);
+		
+		for (int i = 0; i < N; i++) {
+			seg.tree[i + N] = readLong();
+		}
+		seg.init();
 		for (int i = 0; i < M; i++) {
 			if (readInt() == 1) {
-				seg.update(1, 1, N, readInt(), readLong());
+				seg.update(readInt() - 1, readLong());
 			} else {
-				sb.append(seg.query(1, 1, N, readInt(), readInt())).append('\n');
+				sb.append(seg.query(readInt() - 1, readInt() - 1)).append('\n');
 			}
 		}
 		System.out.println(sb);
@@ -56,51 +56,48 @@ public class Main {
 	}
 }
 
-class Seg{
-	private long[] tree;
-	
-	Seg(int n){
-		double treeh = Math.ceil(Math.log(n) / Math.log(2)) + 1;
-		long treesize = Math.round(Math.pow(2, treeh));
-		tree = new long[Math.toIntExact(treesize)];
+class Seg {
+	private int sz , H;
+	public long[] tree;
+
+	Seg(int N) {
+		H = N;
+		sz = N << 1;
+		tree = new long[sz];
 	}
 	
-	void init(long[] arr , int node , int start , int end) {
-		if(start == end)
-			tree[node] = arr[start];
-		else {
-			int mid = (start + end) >> 1;
-			int next = node << 1;
-			init(arr , next , start , mid);
-			init(arr , next + 1 , mid + 1 , end);
-			tree[node] = tree[next] + tree[next + 1];
+	void init(){
+		for (int i = H - 1; i > 0; i--) {
+			int t = i << 1;
+			tree[i] = tree[t] + tree[t + 1];
 		}
 	}
 	
-	void update(int node , int start , int end , int index , long value) {
-		if(index < start || end < index)
-			return;
-		
-		if(index == start && end == index) {
-			tree[node] = value;
-		} else {
-			int mid = (start + end) >> 1;
-			int next = node << 1;
-			update(next , start , mid , index , value);
-			update(next + 1 , mid + 1 , end , index , value);
-			tree[node] = tree[next] + tree[next + 1];
+	void update(int v , long val) {
+		v += H;
+		tree[v] = val;
+
+		for (int i = v >> 1; i > 0; i >>= 1) {
+			int t = i << 1;
+			tree[i] = tree[t] + tree[t + 1];
 		}
 	}
 	
-	long query(int node , int start , int end , int left , int right) {
-		if (end < left || right < start)
-			return 0;
-		else if(left <= start && end <= right) {
-			return tree[node];
-		} else {
-			int mid = (start + end) >> 1;
-			int next = node << 1;
-			return query(next , start , mid , left , right) + query(next + 1 , mid + 1 , end , left , right);
+	long query(int l , int r) {
+		long res = 0;
+		l += H;
+		r += H;
+
+		while(l <= r) {
+			if((l & 1) == 1) {
+				res += tree[l++];
+			}
+			if((r & 1) == 0) {
+				res += tree[r--];
+			}
+			l >>= 1;
+			r >>= 1;
 		}
+		return res;
 	}
 }
