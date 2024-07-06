25a2b3004af091479004e6da987c342a640f0664
1lia <79691058+1lia@users.noreply.github.com>
2024-07-06 19:21:34 +0900

    [Diamond III] Title: 로봇, Time: 664 ms, Memory: 46936 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/17617. 로봇/로봇.java
+++ 백준/Diamond/17617. 로봇/로봇.java
@@ -2,45 +2,33 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int N = readInt();
+        int N = (int) readLong();
         long R = readLong();
         long M = readLong();
-        long[] arr = new long[N];
+        long res = 0;
+        long[] arr = new long[N + 1];
+        long[] arr2 = new long[(N << 1)];
         for (int i = 0; i < N; i++) {
             arr[i] = readLong();
         }
-        Arrays.sort(arr);
-        long res = M - arr[N - 1] + arr[0];
-        for (int i = 1; i < N; i++) {
-            res = Math.max(res , arr[i] - arr[i - 1]);
+        Arrays.sort(arr , 0 , N);
+        arr[N] = arr[0] + M;
+        for (int i = 0; i < N; i++) {
+            arr2[i] = arr2[i + N] = arr[i + 1] - arr[i] - R - R;
         }
-        res -= (R + R);
-        if(res <= 0)
-            System.out.println(0);
-        else System.out.println((res + 1) >> 1);
+        long sum = 0;
+        for (int i = 0; i + 1 < arr2.length; i++) {
+            sum += arr2[i];
+            if(sum < 0)
+                sum = 0;
+            res = Math.max(res , sum);
+        }
+        System.out.println((res + 1) >> 1);
     }
 
     public static long readLong() throws Exception {
         long val = 0;
         long c = System.in.read();
-        while (c <= ' ') {
-            c = System.in.read();
-        }
-        boolean flag = (c == '-');
-        if (flag)
-            c = System.in.read();
-        do {
-            val = 10 * val + c - 48;
-        } while ((c = System.in.read()) >= 48 && c <= 57);
-
-        if (flag)
-            return -val;
-        return val;
-    }
-
-    public static int readInt() throws Exception {
-        int val = 0;
-        int c = System.in.read();
         while (c <= ' ') {
             c = System.in.read();
         }
