3c4edb2eba6d79f88967a272cba39f1eec17ad17
1lia <79691058+1lia@users.noreply.github.com>
2024-07-19 12:05:17 +0900

    [Gold V] Title: 같은 나머지, Time: 80 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Gold/1684. 같은 나머지/같은 나머지.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int N = readInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = readInt();
+        }
+        Arrays.sort(arr);
+        int res = 0;
+        for (int i = 1; i < N; i++) {
+            int t = arr[i] - arr[i - 1];
+            res = gcd(res , t);
+        }
+        System.out.println(res);
+    }
+
+    public static int readInt() throws Exception {
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
+        }
+        boolean flag = (c == '-');
+        if (flag)
+            c = System.in.read();
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+
+        if (flag)
+            return -val;
+        return val;
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
