f2bc9036e1b33b7167e725270e81ff5c86040d22
1lia <79691058+1lia@users.noreply.github.com>
2024-07-30 17:00:51 +0900

    [Diamond IV] Title: Atlantis, Time: 380 ms, Memory: 33672 KB -BaekjoonHub

---
+++ 백준/Diamond/16389. Atlantis/Atlantis.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        PriorityQueue<Long> use = new PriorityQueue<>(Collections.reverseOrder());
+        long N = readLong();
+        while(N-->0){
+            long l = readLong();
+            long d = readLong();
+            if(l <= d)
+                q.offer(new Node(l , d));
+        }
+        int cnt = 0;
+        long size = 0;
+
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            if(size <= node.l){
+                cnt++;
+                size += node.d;
+                use.offer(node.d);
+            } else {
+                long t = use.poll();
+                use.offer(Math.min(t , node.d));
+                size += Math.min(t , node.d) - t;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static long readLong() throws Exception {
+        long val = 0;
+        long c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
+        }
+        boolean flag = (c == '-');
+        if (flag)
+            c = System.in.read();
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+
+        if (flag)
+            return -val;
+        return val;
+    }
+}
+
+class Node implements Comparable<Node>{
+    long l;
+    long d;
+    long e;
+
+    public Node(long l, long d) {
+        this.l = d - l;
+        this.d = l;
+        this.e = d;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if(this.e - o.e < 0){
+            return -1;
+        }
+        return 1;
+    }
+}
